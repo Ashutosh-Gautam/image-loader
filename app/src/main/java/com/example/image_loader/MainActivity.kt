575b5e770735fb19b257ed9cfe92b246f1842964
Ashutosh Gautam <Ashutosh-Gautam@users.noreply.github.com>
2024-10-04 16:03:21 +0530

    Bug fix
    Bug fix

--- app/src/main/java/com/example/image_loader/MainActivity.kt
+++ app/src/main/java/com/example/image_loader/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.image_loader
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
@@ -14,11 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val asyncImageView = findViewById<AsyncImageView>(R.id.asyncImageView)
         val handler = CoroutineExceptionHandler { _, throwable ->
